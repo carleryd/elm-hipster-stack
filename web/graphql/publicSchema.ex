6e2862289c987154d49ca70a320c57e54234bccb
Nils Eriksson <Nilseriksson89.ne@gmail.com>
2016-03-10 23:38:15 +0100

    mutations with links

--- web/graphql/publicSchema.ex
+++ web/graphql/publicSchema.ex
@@ -1,9 +1,14 @@
 defmodule App.PublicSchema do
 
+  import List , only: [first: 1]
   alias GraphQL.Schema
   alias GraphQL.Type.ObjectType
-  alias GraphQL.Mutation
   alias GraphQL.Type.List
+  alias GraphQL.Type.NonNull
+  alias GraphQL.Type.String
+  alias GraphQL.Relay.Mutation
+  alias RethinkDB.Query
+
 
   def schema do
     %Schema{
@@ -16,6 +21,32 @@ defmodule App.PublicSchema do
               %{}
             end
           }
+        }
+      },
+      mutation: %ObjectType{
+        name: "Mutation",
+        fields: %{
+          createLink: Mutation.new(%{
+            name: "createLink",
+            input_fields: %{
+              title: %{type: %NonNull{ofType: %String{}}},
+              url: %{type: %NonNull{ofType: %String{}}}
+            },
+            output_fields: %{
+              link: %{
+                type: App.Type.Link.get,
+                resolve: fn (obj, _args, _info) ->
+                  App.Query.Link.get_link(first(obj[:generated_keys]))
+                end
+              }
+            },
+            mutate_and_get_payload: fn(input, _info) ->
+              Query.table("links")
+                |> Query.insert(%{title: input["title"], url: input["url"]})
+                |> DB.run
+                |> DB.handle_graphql_resp
+            end
+          })
         }
       }
     }
