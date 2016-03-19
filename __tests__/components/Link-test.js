192dbef71a85a6d340cf2f9d70146591da9ee26b
Nils Eriksson <Nilseriksson89.ne@gmail.com>
2016-03-19 17:03:39 +0100

    inital jest tests for Linkcomponent working

--- __tests__/components/Link-test.js
+++ __tests__/components/Link-test.js
@@ -10,21 +10,80 @@ import ReactDOM from 'react-dom';
 import Link from '../../web/static/js/components/Link';
 
 describe('the Link component', () => {
-  describe('the search', () => {
-    it('should display name of the link', () => {
-      const node = {
-        url: 'http://react.is.the.best.com',
-        title: 'Why React?',
-        createdAt: Date.now(),
-      };
+  it('should display name of the tile, url and date http', () => {
+    const link = {
+      url: 'http://react.is.the.best.com',
+      title: 'Why React?',
+      createdAt: 1458396953522,
+    };
 
-      const linkComponent = RelayTestUtils.renderContainerIntoDocument(
-        <Link link={node} />
-      );
-      //
-      // const label = TestUtils.findRenderedDOMComponentWithTag(linkComponent,'a');
-      //
-      // expect(lable.textContent).toEqual(link.title);
-    });
+    const linkComponent = RelayTestUtils.renderContainerIntoDocument(
+      <Link link={link} />
+    );
+
+    const labels = TestUtils
+                   .scryRenderedDOMComponentsWithTag(linkComponent,'a');
+
+    // Title
+    expect(labels[0].textContent).toEqual(link.title);
+    expect(labels[0].getAttribute('href')).toEqual(link.url);
+
+    // URL
+    expect(labels[1].textContent).not.toEqual(link.url);
+    expect(labels[1].textContent).toEqual('react.is.the.best.com');
+    expect(labels[1].getAttribute('href')).toEqual(link.url);
+    // Date
+
+    const date = TestUtils
+                 .findRenderedDOMComponentWithTag(linkComponent,'span');
+
+    expect(date.textContent).toEqual('03/19/2016');
+  });
+
+  it('should test https', () => {
+    const https = {
+      url: 'https://www.facebook.com/',
+      title: 'Facebook',
+      createdAt: 1458300000000,
+    };
+
+    const linkComponent = RelayTestUtils.renderContainerIntoDocument(
+      <Link link={https} />
+    );
+
+    const labels = TestUtils
+                    .scryRenderedDOMComponentsWithTag(linkComponent,'a');
+
+    // Title
+    expect(labels[0].textContent).toEqual(https.title);
+    expect(labels[0].getAttribute('href')).toEqual(https.url);
+
+    // URL
+    expect(labels[1].textContent).not.toEqual(https.url);
+    expect(labels[1].textContent).toEqual('www.facebook.com');
+    expect(labels[1].getAttribute('href')).toEqual(https.url);
+    // Date
+    const date = TestUtils
+                .findRenderedDOMComponentWithTag(linkComponent,'span');
+
+    expect(date.textContent).toEqual('03/18/2016');
+  });
+
+  it('should have optimistic update', () => {
+    const link = {
+      url: 'https://www.optimistic.com/',
+      title: 'Facebook',
+      createdAt: 1458300000000,
+    };
+    const linkComponent = RelayTestUtils.renderContainerIntoDocument(
+      <Link link={link}/>,{
+        hasOptimisticUpdate: true,
+      }
+    );
+    // Date
+    const date = TestUtils
+                .findRenderedDOMComponentWithTag(linkComponent,'span');
+
+    expect(date.textContent).toEqual('Saveing...');
   });
 });
