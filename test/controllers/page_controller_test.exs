d3ec8c09a40059079e330dacd6661c6c634d08ef
Marc Cooper <git@auxbuss.com>
2017-06-21 14:44:17 +0100

    Fix up broken tests.

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule App.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Elm Hipster Stack"
   end
 end
